21e5bd8b37bcd025c1dae79a7cebc14a8e126fd5
vilasreddy444 <vilas.reddy@ntheye.com>
2023-12-11 21:21:34 +0530

    Created express server and UI with header and navigation with react-router

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import  {BrowserRouter,Routes,Route } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import SignOut from './pages/SignUp';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
+import Header from './components/Header';
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+    </Routes>
+   </BrowserRouter>
+  )
+}
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>Home home</div>
+  )
+}
+
+export default Home
