d10eacfd6e70509eb63dd2dff165092deb1807bf
vilasreddy444 <vilas.reddy@ntheye.com>
2023-12-11 21:59:30 +0530

    Created  a connection for mongoose

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config()
 
-const app = express();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to mongoose");
+}).catch((err)=>{
+    console.log(err)
+})
+const app = express(); 
 
 app.listen(3000,()=>{
     console.log("Server is running on 3000 port.")
