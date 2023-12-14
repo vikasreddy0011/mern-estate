59f558a79c84c0aeef93282640cf8139292977ac
vilasreddy444 <vilas.reddy@ntheye.com>
2023-12-14 21:32:40 +0530

    Created signin api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import  jwt from 'jsonwebtoken';
 
 export const signup = async (req,res,next)=>{
     const {username ,email, password} = req.body;
@@ -14,4 +15,23 @@ export const signup = async (req,res,next)=>{
         next(errorHandler(500,err.message))
     }
 
+}
+
+export const Signin = async (req,res,next)=>{
+    const {email,password} = req.body;
+
+    try{
+        var validUser = await User.findOne({email:email})
+        if (!validUser) return next(errorHandler(404,"User doen't exists"));
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword) return next(errorHandler(404,"Password not matching"));
+        const token = jwt.sign({is:validUser._id},process.env.JWT_SECRET)   
+        const {password:pass,...rest} = validUser._doc;
+        res
+        .cookie('access_token',token,{httpOnly: true , expires:new Date(Date.now()+ 24* 60 *60*1000)})
+        .status(200)
+        .json(rest);    
+    }catch(err){
+        next(err);
+    }
 }
